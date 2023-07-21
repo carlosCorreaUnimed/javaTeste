2567127df8c7ebf2adcf728d5852a044e82bc305
carlosCorreaUnimed <carlos.correa@unimedsc.coop.br>
2023-07-21 14:47:05 -0300

    Fazendo o refinamento do código, sem necessidade de orientação a objetos

--- src/desafioBanco/Banco.java
+++ src/desafioBanco/Banco.java
@@ -7,11 +7,10 @@ public class Banco {
 	public static void main(String[] args) {
 		Scanner leitura = new Scanner(System.in);
 		
-		double sInicial = 850.00;
+		double sInicial1= 850.00;
 		int i = 0;
+		System.out.println("Seu saldo inicial é de R$%.2f".formatted(sInicial1));
 		while (i == 0) {
-		
-		System.out.println("Seu saldo inicial é de R$%.2f".formatted(sInicial));
 		System.out.print("""
 
 				1 - Consultar Saldo
@@ -19,29 +18,36 @@ public class Banco {
 				3 - Transferir Valor
 				4 - Sair
 				
-				O que deseja fazer?
+				O que você deseja fazer?
 				""");
 
 		int digita = leitura.nextInt();
 		switch (digita) {
 		   case 1:
-		      System.out.println("\nSeu saldo é de: " + sInicial);
+		      System.out.println("\nSeu saldo é de: " + sInicial1);
 		      break;
 		   case 2:
 		      System.out.println("\nQuanto você irá receber? ");
 		      double receber = leitura.nextDouble();
-		      sInicial += receber;
-		      System.out.println("\nSaldo atualizado %.2f:".formatted(sInicial));
+		      if(receber < 0) {
+		    	  System.out.println("\nValor Inválido, tente novamente");
+		    	  break;
+		      }
+		      sInicial1 += receber;
+		      System.out.println("\nSaldo atualizado %.2f:".formatted(sInicial1));
 		      break;
 		   case 3:
 		      System.out.println("\nQuanto você irá transferir? ");
 		      double transferir = leitura.nextDouble();
-		      if(transferir > sInicial) {
+		      if(transferir > sInicial1) {
 		    	  System.out.println("\nFalta de saldo para transferência!");
 		    	  break;
+		      }else if(transferir < 0) {
+		    	  System.out.println("\nValor inválido, tente novamente");
+		    	  break;
 		      }
-		      sInicial -= transferir;
-		      System.out.println("\nSaldo atualizado %.2f:".formatted(sInicial));
+		      sInicial1 -= transferir;
+		      System.out.println("\nSaldo atualizado %.2f:".formatted(sInicial1));
 		      break;
 		   case 4:
 			      System.out.println("\nVocê saiu do aplicativo");
