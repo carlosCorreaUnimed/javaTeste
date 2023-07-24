066ab2a887c64d66cd5e01a31deb606e05476a24
carlosCorreaUnimed <carlos.correa@unimedsc.coop.br>
2023-07-24 14:27:48 -0300

    Realizada as mudanças no código passadno as fo´rmulas para a classe Clientes

--- src/desafioBanco/Banco.java
+++ src/desafioBanco/Banco.java
@@ -15,14 +15,34 @@ public class Banco {
 		//double pessoa1.getSaldo()= 850.00;
 		System.out.println("Escolha com qual usuário você quer entrar: ");
 		int escolher = leitura.nextInt();
+		
 		int i = 0;
-		if (escolher == 1) {
+		
+		int u = 0;
+		
+		while(u == 0) {
+		switch (escolher) {
+		case 1: {
 			System.out.println("Seu saldo inicial é de R$%.2f".formatted(pessoa1.getSaldo()));
-		}else if (escolher == 2) {
-			System.out.println("Seu saldo inicial é de R$%.2f".formatted(pessoa2.getSaldo()));
-		}else {
-			System.out.println("Essa conta não existe");
+			u++;
+			break;
 		}
+		
+		case 2: {
+			System.out.println("Seu saldo inicial é de R$%.2f".formatted(pessoa2.getSaldo()));
+			u++;
+			break;
+		}
+		
+		default:
+			System.out.println("Essa conta não existe");
+			System.out.println("Digite 1 ou 2: ");
+			escolher = leitura.nextInt();
+			break;
+			
+		}
+		}
+
 		while (i == 0) {
 		System.out.print("""
 
@@ -37,9 +57,8 @@ public class Banco {
 		int digita = leitura.nextInt();
 		switch (digita) {
 		   case 1:
-		      System.out.println("\nSeu saldo é de: " + pessoa1.getSaldo());
+			  pessoa1.consulta(pessoa1, pessoa2, escolher);
 		      break;
-		      
 		   case 2:
 			   
 		      System.out.println("\nQuanto você irá receber? ");
@@ -56,7 +75,7 @@ public class Banco {
 		      System.out.println("\nQuanto você irá transferir? ");
 		      double transferir = leitura.nextDouble();
 		      try {
-		    	  pessoa1.transferir(escolher, transferir, pessoa2);
+		    	  pessoa1.transferir(escolher, transferir, pessoa2, pessoa1);
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
--- src/desafioBanco/Clientes.java
+++ src/desafioBanco/Clientes.java
@@ -63,9 +63,13 @@ public class Clientes {
 		}
 	}
 
-	public void transferir(int escolher, double transferir, Clientes pessoa2) {
+	public void transferir(int escolher, double transferir, Clientes pessoa2, Clientes pessoa1) {
+		boolean conta1 = this.contaBloqueada;
+		boolean conta2 = pessoa2.contaBloqueada;
 		if(escolher == 1) {
-			if(transferir > getSaldo()) {
+			if(conta2 || conta1 == true) {
+				  throw new IllegalArgumentException("Conta Bloqueada!");
+		}else if(transferir > getSaldo()) {
 		    	  //System.out.println("\nFalta de saldo para transferência!");
 		    	  throw new IllegalArgumentException("Falta de saldo para tranferência");
 		      }else if(transferir < 0) {
@@ -77,25 +81,34 @@ public class Clientes {
 			double saldoTransferido = pessoa2.getSaldo() + transferir;
 			pessoa2.setSaldo(saldoTransferido);
 			System.out.println("\nSeu saldo foi atualizado para %.2f, você enviou %.2f para %s, totalizando %.2f:".formatted(this.getSaldo(), transferir, pessoa2.getNome(), pessoa2.getSaldo()));
+			
 	  }}else if (escolher == 2){
-		
-		if(transferir > getSaldo()) {
+		  if(conta2 || conta1 == true) {
+			  throw new IllegalArgumentException("Conta Bloqueada!");
+		  }else if(transferir > getSaldo()) {
 	    	  //System.out.println("\nFalta de saldo para transferência!");
 	    	  throw new IllegalArgumentException("Falta de saldo para tranferência");
 	      }else if(transferir < 0) {
 	    	  //System.out.println("\nValor inválido, tente novamente");
 	    	  throw new IllegalArgumentException("Valor inválido, tente novamente");
 	      }else {
-		double saldoDesconto = this.getSaldo() - transferir;
-		this.setSaldo(saldoDesconto);
-		double saldoTransferido = pessoa2.getSaldo() + transferir;
-		pessoa2.setSaldo(saldoTransferido);
-		System.out.println("\nSeu saldo foi atualizado para %.2f, você enviou %.2f para %s, totalizando %.2f:".formatted(this.getSaldo(), transferir, pessoa2.getNome(), pessoa2.getSaldo()));
+		double saldoDesconto = pessoa2.getSaldo() - transferir;
+		pessoa2.setSaldo(saldoDesconto);
+		double saldoTransferido = this.getSaldo() + transferir;
+		this.setSaldo(saldoTransferido);
+		System.out.println("\nSeu saldo foi atualizado para %.2f, você enviou %.2f para %s, totalizando %.2f:".formatted(pessoa2.getSaldo(), transferir, this.getNome(), this.getSaldo()));
 	      }
-	  }else {
+		  }else {
 		  System.out.println("Essa conta não existe");
-	  }
-
-	
-}
 	}
+}
+
+	public void consulta(Clientes pessoa1, Clientes pessoa2, int escolher) {
+		if(escolher == 1) {
+			System.out.println("\nSeu saldo é de %.2f ".formatted(pessoa1.getSaldo()));
+		}else if (escolher == 2) {
+			System.out.println("\nSeu saldo é de %.2f ".formatted(pessoa2.getSaldo()));
+		}
+		
+	}
+}
